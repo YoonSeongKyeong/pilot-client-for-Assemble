a91f77c730ea5e4fd08dfea547947c122e13aaf7
YoonSeongKyeong <mdy6099@naver.com>
2020-01-17 15:23:55 +0900

    init with external component and modules

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Route, Switch} from 'react-router-dom';
+import { Home } from './pages';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {};
+
+  render() {
+    return (
+      <div>
+        <Route exact path="/" component={() =>Home} />
+        <Switch>
+          <Route path="/home" component={() => Home} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
